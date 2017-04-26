43a21e5e7e82524d71a9097da7a655cad26f4ec9
Thijs Scheepers <thijs@label305.com>
2017-04-26 21:06:38 +0200

    Running script

--- src/main.py
+++ src/main.py
@@ -22,7 +22,7 @@ def main():
     ns_per_v = defaultdict(list)
     vs_per_n = defaultdict(list)
     f_vn = defaultdict(int)
-    ys = set
+    ys = []
 
     for vt, nt in corpus:
         ns_per_v[vt] += [nt]
@@ -32,10 +32,11 @@ def main():
 
     # Number of classes
     n_cs = 30
-    ns = list(set(ns_per_v.keys()))
-    vs = list(set(vs_per_n.keys()))
-    n_unique_vs = len(ns)
-    n_unique_ns = len(vs)
+    ys = list(set(ys))
+    ns = list(set(vs_per_n.keys()))
+    vs = list(set(ns_per_v.keys()))
+    n_unique_vs = len(vs)
+    n_unique_ns = len(ns)
     n_unique_ys = len(ys)
 
     # Theta, parameters
@@ -46,7 +47,7 @@ def main():
     p_nc = np.random.rand(n_unique_ns, n_cs)
     p_nc /= np.sum(p_nc, axis=0)
 
-    def p(c, v, n):
+    def p_c_vn(c, v, n):
 
         if isinstance(v, str):
             v = vs.index(v)
@@ -54,7 +55,11 @@ def main():
         if isinstance(n, str):
             n = ns.index(n)
 
-        return p_c[c] * p_vc[v, c] * p_nc[n, c]
+        return p_c[c] * p_vc[v, c] * p_nc[n, c] / p_vn(v, n)
+
+    def p_vn(v, n):
+
+        return sum([p_c[c] * p_vc[v, c] * p_nc[n, c] for c in range(n_cs)])
 
     def f(v, n):
 
@@ -70,27 +75,32 @@ def main():
     for i in range(10):
         p_c_1 = np.array(p_c)
         p_vc_1 = np.array(p_vc)
-        p_nc_1 = defaultdict(p_nc)
+        p_nc_1 = np.array(p_nc)
+
+        likelihood = 0
 
         for c in range(n_cs):
 
             # Sigma_y f(y)p(x|y)
-            d = sum([f(v, n) * p(c, v, n) for (v, n) in ys])
+            d = sum([f(v, n) * p_c_vn(c, v, n) for (v, n) in ys])
+            likelihood += np.log(d)
 
             for v, vt in enumerate(vs):
-                p_vc_1[v, c] = sum([f(v, n) * p(c, v, n) for n in ns_per_v[vt]]) / d
+                p_vc_1[v, c] = sum([f(v, n) * p_c_vn(c, v, n) for n in ns_per_v[vt]]) / d
 
             for n, nt in enumerate(ns):
-                p_nc_1[n, c] = sum([f(v, n) * p(c, v, n) for v in vs_per_n[nt]]) / d
+                p_nc_1[n, c] = sum([f(v, n) * p_c_vn(c, v, n) for v in vs_per_n[nt]]) / d
 
             p_c_1[c] = d
+
+        print('Likelihood: %f', likelihood)
 
         p_c_1 /= n_unique_ys
 
         p_c = p_c_1
         p_vc = p_vc_1
         p_nc = p_nc_1
-        
+
 
 if __name__ == "__main__":
     main()
