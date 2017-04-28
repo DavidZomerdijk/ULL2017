767beed0738c21274a9bf80be9dc2793adb7b705
Thijs Scheepers <thijs@label305.com>
2017-04-28 16:00:35 +0200

    Modified main.py, and cleaned up some stuff

--- src/main.py
+++ src/main.py
@@ -1,15 +1,225 @@
 # coding: utf-8
 from collections import defaultdict
 from os import path
-
+from sys import stdout
 import numpy as np
+import pickle
 
 
-def read_corpus(path):
-    """Read a file as a list of tuples"""
+class Dataset:
+    """
+    This class contains all needed information from the dataset
+    neatly structured
+    """
 
-    with open(path, 'r') as f:
-        return [tuple(ln.strip().split()) for ln in f]
+    @classmethod
+    def load(cls, file_path, max_lines=None):
+        """
+        Loads .pkl file if available, otherwise creates dataset
+        :return: Dataset
+        """
+
+        pickle_path = cls.pickle_path(file_path, max_lines=max_lines)
+
+        if path.isfile(pickle_path):
+            print("Loading dataset (%s) from disk" % pickle_path)
+            dataset = pickle.load(open(pickle_path, 'rb'))
+        else:
+            dataset = Dataset(file_path, max_lines=max_lines)
+
+        print("Dataset ready")
+        print("\tUnique verbs:\t%d" % dataset.n_vs)
+        print("\tUnique nouns:\t%d" % dataset.n_ns)
+        print("\tUnique pairs:\t%d" % dataset.n_ys)
+
+        return dataset
+
+    @classmethod
+    def pickle_path(cls, file_path, max_lines=None):
+        """
+        Returns the path of the pickle file
+        """
+
+        if max_lines is None:
+            return file_path + '.pkl'
+        else:
+            return file_path + '-%d.pkl' % max_lines
+
+    def __init__(self, file_path, max_lines=None):
+        """
+        Initialize a Dataset and read it in
+        """
+
+        lines = self.read_lines(file_path)
+
+        # Token-index lookup
+        self.ns = list()
+        self.vs = list()
+
+        self.ns_per_v = defaultdict(set)
+        self.vs_per_n = defaultdict(set)
+        self.f_vn = defaultdict(int)
+        self.ys = set()
+
+        if max_lines is None or max_lines > len(lines):
+            n_lines = len(lines)
+        else:
+            n_lines = max_lines
+
+        for i, ln in enumerate(lines):
+
+            if i > n_lines:
+                break
+
+            if i % 1000 == 0:
+                stdout.write("\rReading corpus… %6.2f%%" % ((100 * i) / float(n_lines),))
+                stdout.flush()
+
+            if len(ln) == 2:
+                vt, nt = ln
+
+                if nt not in self.ns:
+                    n = len(self.ns)
+                    self.ns.append(nt)
+                else:
+                    n = self.ns.index(nt)
+
+                if vt not in self.vs:
+                    v = len(self.vs)
+                    self.vs.append(vt)
+                else:
+                    v = self.vs.index(vt)
+
+                self.ns_per_v[v] |= {n}
+                self.vs_per_n[n] |= {v}
+                self.ys |= {(v, n)}
+                self.f_vn[(v, n)] += 1
+
+        # Lengths
+        self.n_vs = len(self.vs)
+        self.n_ns = len(self.ns)
+        self.n_ys = len(self.ys)
+
+        print("\rDataset read")
+
+        self.store(file_path, max_lines)
+
+    def store(self, file_path, max_lines):
+        """
+        Stores dataset to .pkl file
+        """
+        pickle.dump(self, open(Dataset.pickle_path(file_path, max_lines=max_lines), 'wb'))
+
+    def read_lines(self, file_path):
+        """
+        Read a file as a list of tuples
+        """
+        with open(file_path, 'r') as f:
+            return [tuple(ln.strip().split()) for ln in f]
+
+
+class LSCVerbClasses:
+    """
+    Latent Semantic Clustering for Verb Classes
+
+    This is the implementation of Step 1
+    Verb classes (corresponding to Section 2 in the paper https://arxiv.org/abs/cs/9905008)
+    """
+
+    def __init__(self, dataset, n_cs=30, em_iters=50):
+        """
+        :param dataset: The dataset for which to train
+        :param n_cs: Number of classes
+        :param em_iters: Iterations
+        """
+        self.dataset = dataset
+        self.n_cs = n_cs
+        self.em_iters = em_iters
+
+        self.p_vn = None  # Calculated each EM-Iteration
+        self.p_c, self.p_vc, self.p_nc = self.initialize_parameters()
+
+    def initialize_parameters(self):
+        """
+        Initialize theta parameters
+        :return:
+        """
+        p_c = np.random.rand(self.n_cs)
+        p_c /= np.sum(p_c)
+        p_vc = np.random.rand(self.dataset.n_vs, self.n_cs)
+        p_vc /= np.sum(p_vc, axis=0)
+        p_nc = np.random.rand(self.dataset.n_ns, self.n_cs)
+        p_nc /= np.sum(p_nc, axis=0)
+
+        return p_c, p_vc, p_nc
+
+    def p_c_vn(self, c, v, n):
+        """
+        p(c|v, n)
+        """
+
+        return self.p_c[c] * self.p_vc[v, c] * self.p_nc[n, c] / self.p_vn[(v, n)]
+
+    def f(self, v, n):
+        """
+        frequency for pair
+        f(v, n)
+        """
+
+        return self.dataset.f_vn[(v, n)]
+
+    def train(self):
+        """
+        Train the algorithm
+        """
+        for i in range(self.em_iters):
+            self.em_iter(i)
+
+    def em_iter(self, i=0):
+        """
+        Do an EM step
+        :param i: iteration
+        :return: log-likelihood
+        """
+
+        p_c_1 = np.array(self.p_c)
+        p_vc_1 = np.array(self.p_vc)
+        p_nc_1 = np.array(self.p_nc)
+
+
+        self.p_vn = { (v, n):
+                      sum([self.p_c[c] * self.p_vc[v, c] * self.p_nc[n, c] for c in range(self.n_cs)])
+                      for (v, n)
+                      in self.dataset.ys
+                    }
+
+        likelihood = sum([np.log(self.f(v, n) * self.p_vn[(v, n)]) for (v, n) in self.dataset.ys])
+        print('%i: Log-likelihood: %f' % (i, likelihood))
+
+        for c in range(self.n_cs):
+
+            # Sigma_y f(y)p(x|y)
+            d = sum([self.f(v, n) * self.p_c_vn(c, v, n) for (v, n) in self.dataset.ys])
+
+            for v in range(self.dataset.n_vs):
+                # Sigma_y in {v} X N f(y)p(x|y)
+                p_vc_1[v, c] = sum([self.f(v, n) * self.p_c_vn(c, v, n) for n in self.dataset.ns_per_v[v]]) / d
+
+            for n in range(self.dataset.n_ns):
+                # Sigma_y in N X {v} f(y)p(x|y)
+                p_nc_1[n, c] = sum([self.f(v, n) * self.p_c_vn(c, v, n) for v in self.dataset.vs_per_n[n]]) / d
+
+            p_c_1[c] = d
+
+        # d / |Y|
+        p_c_1 /= self.dataset.n_ys
+
+        self.p_c = p_c_1
+        self.p_vc = p_vc_1
+        self.p_nc = p_nc_1
+
+        return likelihood
+
 
 def main():
     """Program entry point"""
@@ -17,103 +227,10 @@ def main():
     data_path = path.join(path.dirname(__file__), '..', 'data')
     gold_corpus = path.join(data_path, 'gold_deps.txt')
     all_pairs = path.join(data_path, 'all_pairs')
-    corpus = read_corpus(gold_corpus)
 
-    ns_per_v = defaultdict(list)
-    vs_per_n = defaultdict(list)
-    f_vn = defaultdict(int)
-    ys = []
+    dataset = Dataset.load(all_pairs, max_lines=500000)
 
-    for vt, nt in corpus:
-        ns_per_v[vt] += [nt]
-        vs_per_n[nt] += [vt]
-        ys += [(vt, nt)]
-        f_vn[(vt, nt)] += 1
-
-    # Number of classes
-    n_cs = 30
-
-    # Iterations
-    em_iter = 20
-
-    ys = list(set(ys))
-    ns = list(set(vs_per_n.keys()))
-    vs = list(set(ns_per_v.keys()))
-    n_unique_vs = len(vs)
-    n_unique_ns = len(ns)
-    n_unique_ys = len(ys)
-
-    # Theta, parameters
-    p_c = np.random.rand(n_cs)
-    p_c /= np.sum(p_c)
-    p_vc = np.random.rand(n_unique_vs, n_cs)
-    p_vc /= np.sum(p_vc, axis=0)
-    p_nc = np.random.rand(n_unique_ns, n_cs)
-    p_nc /= np.sum(p_nc, axis=0)
-
-    # p(c|v, n)
-    def p_c_vn(c, v, n):
-
-        if isinstance(v, str):
-            v = vs.index(v)
-
-        if isinstance(n, str):
-            n = ns.index(n)
-
-        return p_c[c] * p_vc[v, c] * p_nc[n, c] / p_vn(v, n)
-
-    # p(v, n)
-    def p_vn(v, n):
-
-        if isinstance(v, str):
-            v = vs.index(v)
-
-        if isinstance(n, str):
-            n = ns.index(n)
-
-        return sum([p_c[c] * p_vc[v, c] * p_nc[n, c] for c in range(n_cs)])
-
-    # frequency for pair
-    def f(v, n):
-
-        if isinstance(v, int):
-            v = vs[v]
-
-        if isinstance(n, int):
-            n = ns[n]
-
-        return f_vn[(v, n)]
-
-    # EM iterations
-    for i in range(em_iter):
-        p_c_1 = np.array(p_c)
-        p_vc_1 = np.array(p_vc)
-        p_nc_1 = np.array(p_nc)
-
-        likelihood = sum([np.log(f(v, n) * p_vn(v, n)) for (v, n) in ys])
-        print('%i: Log-likelihood: %f' % (i, likelihood))
-
-        for c in range(n_cs):
-
-            # Sigma_y f(y)p(x|y)
-            d = sum([f(v, n) * p_c_vn(c, v, n) for (v, n) in ys])
-
-            for v, vt in enumerate(vs):
-                # Sigma_y in {v} X N f(y)p(x|y)
-                p_vc_1[v, c] = sum([f(v, n) * p_c_vn(c, v, n) for n in ns_per_v[vt]]) / d
-
-            for n, nt in enumerate(ns):
-                # Sigma_y in N X {v} f(y)p(x|y)
-                p_nc_1[n, c] = sum([f(v, n) * p_c_vn(c, v, n) for v in vs_per_n[nt]]) / d
-
-            p_c_1[c] = d
-
-        p_c_1 /= n_unique_ys
-
-        p_c = p_c_1
-        p_vc = p_vc_1
-        p_nc = p_nc_1
-
+    LSCVerbClasses(dataset, n_cs=30, em_iters=50).train()
 
 if __name__ == "__main__":
     main()
