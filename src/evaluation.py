3161f7baeb34673e3fde02141d90faaba6e8abaf
davidzomerdijk <david.zomerdijk93@gmail.com>
2017-05-01 21:49:03 +0200

    evaluation 3.1, evaluation kinda working. (still 0.4 acc. after 0 iterations)

--- src/evaluation.py
+++ src/evaluation.py
@@ -4,7 +4,10 @@ from os import path
 import random
 import pickle
 import numpy as np
-import main
+
+from main import LSCVerbClasses, Dataset
+
+
 
 #create eval and train set
 
@@ -91,14 +94,14 @@ def train_models():
     path_models = "../data/models"
     path_train_data = "../data/train_eval"
     name_train_data = "gold_corpus_train_1000.txt"
-    train_data = main.Dataset.load( path.join(path_train_data, name_train_data) )
+    train_data = Dataset.load( path.join(path_train_data, name_train_data) )
 
     #parameters is a tuple of (number of classes, number of iterations)
     parameters = [(15,20),(40,50) ]
 
 
     for param in parameters:
-        model = main.LSCVerbClasses(train_data, n_cs=param[0], em_iters=param[1])
+        model = LSCVerbClasses(train_data, n_cs=param[0], em_iters=param[1],name='gold_deps')
         model.train()
         outputName = path.join(path_models, (name_train_data[:-4] + "_c=" + str(param[0]) + "i=_" + str(param[1]) + ".p" ))
         pickle.dump(model, open(outputName, "wb"))
@@ -107,28 +110,68 @@ def train_models():
 
 def evaluate_models(modelFiles, evaluationFile):
     evaluation = pickle.load(open(evaluationFile, 'rb'))
+    path_models = "../data/models"
+    path_train_data = "../data/train_eval"
+    name_train_data = "gold_corpus_train_1000.txt"
+    train_data = Dataset.load(path.join(path_train_data, name_train_data))
+
+    idx_vs = dict()
+    idx_ns = dict()
+    for i in range(len(train_data.ns)):
+        idx_ns[ train_data.ns[i]] = i
+    for i in range(len(train_data.vs)):
+        idx_vs[ train_data.vs[i]] = i
 
     for modelfile in modelFiles:
         model = pickle.load(open(modelfile, 'rb'))
-
-        print(model.p_vc)
         count_biggerthen = 0
+        for v, n, v_prime in evaluation:
+            idx_n = idx_ns[n]
+            idx_v = idx_vs[v]
+            idx_v_prime  = idx_vs[v_prime]
+
+            # print(idx_n)
+            # print(idx_v)
+
+            denominator = sum( model.p_c ) * model.p_nc.shape[0] + np.sum(model.p_vc[idx_v] )  * model.p_nc.shape[0] + np.sum( model.p_nc )
+
+            numerator = sum( model.p_c )  + np.sum(model.p_vc[idx_v,:])   + sum( model.p_nc[idx_n ,:])
+            numerator_prime = sum( model.p_c ) + np.sum(model.p_vc[idx_v,:])   + sum( model.p_nc[idx_n ,:])
+            denominator_prime = sum(model.p_c) * model.p_nc.shape[0] + np.sum(model.p_vc[idx_v_prime]) * model.p_nc.shape[0] + np.sum(
+                model.p_nc)
+
+            # print("pc: ",sum( model.p_c) )
+            # print("shape p_vc: " ,model.p_vc.shape)
+            # print("p_vc: ", sum(model.p_vc[idx_v,:]))
+            # print("p_nc", sum( model.p_nc[idx_n, : ]))
+            # print(" numerator: ", numerator)
+            # print("denominator", denominator)
+            # print("sum( model.p_nc ): ",  model.p_nc.shape )
+            # self.p_c, self.p_vc, self.p_nc
+
+            P_n_given_v = numerator / denominator
+            P_n_given_v_prime = numerator_prime / denominator_prime
+
+            if P_n_given_v > P_n_given_v_prime:
+                count_biggerthen += 1
+
+
+
+
+
+        print( "Accuracy: " + str(count_biggerthen * 1.0 / len(evaluation)))
 
         # for v, n in evaluation:
-
-
-
 
     return
 
 
 
-
 if __name__ == "__main__":
-    # create_datasets()
-    # train_models()
+    create_datasets()
+    train_models()
 
-    modelfiles = ["../data/models/gold_corpus_train_1000_c=15i=_20.p", "../data/models/gold_corpus_train_1000_c=40i=_50.p"]
+    modelfiles = ["../out/gold_deps-30-0.pkl", "../out/gold_deps-30-30.pkl", "../out/gold_deps-30-49.pkl"]
     evaluationFile = "../data/train_eval/gold_corpus_eval_1000.p"
     evaluate_models(modelfiles, evaluationFile)
 
