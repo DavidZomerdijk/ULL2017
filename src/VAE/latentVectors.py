697f5add28b182e5ea158a851160aa4b00c6709c
davidzomerdijk <david.zomerdijk93@gmail.com>
2017-05-29 11:36:53 +0200

    File started implementing efficient sparse matrices

--- src/VAE/latentVectors.py
+++ src/VAE/latentVectors.py
@@ -11,6 +11,7 @@ import os
 from sklearn.cluster import KMeans
 from collections import defaultdict
 
+
 np.random.seed(42)
 hu_encoder = 400
 hu_decoder = 400
@@ -55,19 +56,24 @@ def train_VAE():
             model.save_parameters(path)
 
 def create_train_matrix(dataset):
-    x_train = []
-    for i in dataset.train_VAE:
-        verb_temp = np.zeros(len(dataset.vs))
-        verb_temp.itemset(i[0],int(1))
+    row = np.array([0, 0, 1, 2, 2, 2])
+    col = np.array([0, 2, 2, 0, 1, 2])
+    data = np.array([1, 2, 3, 4, 5, 6])
+    mtx = sparse.csc_matrix((data, (row, col)), shape=(3, 3))
 
-        verb_suf_temp = np.zeros(len(dataset.vps))
-        verb_suf_temp.itemset(i[1],int(1))
-
-        noun_temp = np.zeros(len(dataset.ns))
-        noun_temp.itemset(i[2],int(1))
-
-        x_train.append( np.concatenate((verb_temp, verb_suf_temp, noun_temp)))
-    return np.array(x_train)
+    # x_train = []
+    # for i in dataset.train_VAE:
+    #     verb_temp = np.zeros(len(dataset.vs))
+    #     verb_temp.itemset(i[0],int(1))
+    #
+    #     verb_suf_temp = np.zeros(len(dataset.vps))
+    #     verb_suf_temp.itemset(i[1],int(1))
+    #
+    #     noun_temp = np.zeros(len(dataset.ns))
+    #     noun_temp.itemset(i[2],int(1))
+    #
+    #     x_train.append( np.concatenate((verb_temp, verb_suf_temp, noun_temp)))
+    # return np.array(x_train)
 
 def cluster_hidden_vectors(hidden_vectors, dataset):
     kmeans = KMeans(n_clusters = 30, random_state=0 ).fit(hidden_vectors)
@@ -97,7 +103,7 @@ if __name__ == "__main__":
 
     model = VAE(continuous, hu_encoder, hu_decoder, n_latent, X_VAE)
 
-    trainModel = True
+    trainModel = False
     if(trainModel):
         train_VAE()
     else:
