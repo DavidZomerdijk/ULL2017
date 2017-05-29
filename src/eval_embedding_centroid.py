42748abb876fcfb18c1cabeeffeea07b9fa28047
Thijs Scheepers <thijs@label305.com>
2017-05-29 11:43:07 +0200

    added eval stub

---
+++ src/eval_embedding_centroid.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+import random
+
+
+class EvaluationEmbeddingCentroids:
+    """
+    Evaluation using embeddings and cluster centroids in embedding space
+    """
+
+    def __init__(self, dataset, model):
+        pass
